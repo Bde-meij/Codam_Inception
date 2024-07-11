0737c8246ee6f183f8e46947a1832f07ed6426e7
bde-meij <bde-meij@bde-meij.42.fr>
2024-07-11 12:52:57 +0200

    hand-in version

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -16,38 +16,3 @@ fi
 
 # Run mariadbd
 mariadbd
-
-# if [ -f /var/lib/mysql/initialized ]
-# then
-# 	touch /var/lib/mysql/initialized
-# else
-# 	mysql_install_db --user=mysql --datadir=/var/lib/mysql
-# 	mysqld & sleep 5
-# 	mysql -e "CREATE DATABASE IF NOT EXISTS $WORDPRESSDATABASE;"
-# 	mysql -e "CREATE USER IF NOT EXISTS '$admin'@'%' IDENTIFIED BY '$adminpass';"
-# 	mysql -e "GRANT ALL PRIVILEGES ON $WORDPRESSDATABASE.* TO $admin@localhost IDENTIFIED BY '$WORDPRESSDATABASEPASSWD'"
-# 	mysql -e "FLUSH PRIVILEGES;"
-# 	killall mysqld
-# 	touch /var/lib/mysql/initialized
-# fi
-# mysqld
-
-# If the database doesn't already exist initialize the database and users
-# if [ ! -d "/var/lib/mysql/$WORDPRESSDATABASE" ]; then
-	# mysql_install_db --user=mysql --datadir=/var/lib/mysql
-	# mysqld & sleep 5
-	# mysql -e "CREATE DATABASE IF NOT EXISTS \`$WORDPRESSDATABASE\`;"
-	# mysql -e "CREATE USER IF NOT EXISTS '$WORDPRESSDATABASEUSER'@'%' IDENTIFIED BY '$WORDPRESSDATABASEPASSWD';"
-	# mysql -e "GRANT ALL PRIVILEGES ON \`$WORDPRESSDATABASE\`.* TO '$WORDPRESSDATABASEUSER'@'%' IDENTIFIED BY '$WORDPRESSDATABASEPASSWD';"
-	# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '1';"
-	# mysql -e "FLUSH PRIVILEGES;"
-
-	# cat init.sql
-
-	# run mysqld while giving init.sql as input to initialize the database
-	# killall mysqld
-	# mysqld --bootstrap < init.sql
-# fi
-
-# Run mysqld
-# mysqld
